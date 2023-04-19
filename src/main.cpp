5c607cb220db40e1ddbe49a7c1ba779ee40b10bf
kinly <kinly.lei@gmail.com>
2023-04-19 09:59:32 +0800

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@ int main()
 {
     util::logger::easy_logger<util::logger::key_default>::get().init(util::logger::options_default);
 
-	PRINT_WARN(util::logger::key_default, "warn log, %d-%d", 1, 2);
+    PRINT_WARN(util::logger::key_default, "warn log, %d-%d", 1, 2);
 
-	return 0;
+    return 0;
 }
